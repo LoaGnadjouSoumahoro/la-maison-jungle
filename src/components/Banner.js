963bf94cfe7acebcb2231a5911aa061c71a20773
loa gnadjou soumahoro <loagnadjou@gmail.com>
2024-02-10 10:11:56 +0100

    Ajout du logo et du style aux components header et cart

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,13 @@
+import "../styles/Banner.css";
+import logo from "../assets/logo_la_maison_jungle.png";
 function Banner() {
-  return <h1>La maison de la jungle</h1>;
+  const title = "La maison de la jungle";
+  return (
+    <div className="lmj-banner">
+      <img src={logo} alt="La maison Jungle" className="lmj-logo" />
+      <h1 className="lmj-title">{title}</h1>
+    </div>
+  );
 }
 
 export default Banner;
